ccd878eadd9f6e811caf088311862911e2cbb71f
vikas <vikaskumar783588@gmail.com>
2025-10-28 21:18:56 +0530

    fixing build errors

--- app/src/app/api/dynamic-nft-key/[key]/route.ts
+++ app/src/app/api/dynamic-nft-key/[key]/route.ts
@@ -4,9 +4,10 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(
   req: NextRequest,
-  { params }: { params: { key: string } }
+  { params }: { params: Promise<{ key: string }> }
 ) {
-  const userPubKey = new PublicKey(params.key);
+  const paramsValue = await params;
+  const userPubKey = new PublicKey(paramsValue.key);
   // const nftPubKey = await getKv(params.key);
   const nftMintAddress = await getKv(`${userPubKey}-nftMintAddress`);
   return NextResponse.json({ nftMintAddress });
